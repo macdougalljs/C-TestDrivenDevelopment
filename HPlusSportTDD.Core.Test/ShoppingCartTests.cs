fe56fd204f41b76544a9a11628dcf635d72fbd9b
Jeremy MacDougall <macdojer@outlook.com>
2022-11-26 13:08:39 -0500

    Adding another test

--- HPlusSportTDD.Core.Test/ShoppingCartTests.cs
+++ HPlusSportTDD.Core.Test/ShoppingCartTests.cs
@@ -35,5 +35,42 @@ namespace HPlusSportTDD.Core
             Assert.Contains(item, response.Items);
 
         }
+
+        [Test]
+        public void ShouldReturnArticlesAddedToCart()
+        {
+            var item = new AddToCartItem()
+            {
+                ArticleId = 42,
+                Quantity = 5
+            };
+           
+            var request = new AddToCartRequest()
+            {
+                Item = item
+            };
+
+            
+            var manager = new ShoppingCartManager();
+
+            AddToCartResponse response = manager.AddToCart(request);
+
+            var item2 = new AddToCartItem()
+            {
+                ArticleId = 43,
+                Quantity = 10
+            };
+
+            request = new AddToCartRequest()
+            {
+                Item = item2
+            };
+
+
+            Assert.NotNull(response);
+            Assert.Contains(item, response.Items);
+            Assert.Contains(item2, response.Items);
+
+        }
     }
 }
